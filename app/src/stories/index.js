c640159f0e7e94492538e5a4ae161c767f668189
uppermanis <smrt.local@gmail.com>
2018-01-08 21:30:02 +0200

    Update storybook

--- app/src/stories/index.js
+++ app/src/stories/index.js
@@ -6,12 +6,12 @@ import { linkTo } from '@storybook/addon-links';
 
 import { Button, Welcome } from '@storybook/react/demo';
 import Table from '../components/Table';
-import { dataSource, columns } from '../constants/data';
+import { dataSource, dataSourceWithFixed, columns, columnsWithFixed} from '../constants/data';
 
 storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
 
 storiesOf('Table', module)
   .add('Default', () => <Table dataSource={dataSource} columns={columns} />)
-  .add('with some emoji', () => <Button onClick={action('clicked')}>😀 😎 👍 💯</Button>);
+  .add('With fixed columns', () => <Table dataSource={dataSourceWithFixed} columns={columnsWithFixed} />);
 
 
